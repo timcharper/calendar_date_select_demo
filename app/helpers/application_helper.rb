bc6678887b0b625f92c46208a87348d51fe4fc0d
Tim Harper <timcharper@gmail.com>
2010-03-25 14:57:57 -0600

    helper to reduce redundancy between evaluated code and display code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def eval_and_show_code(code)
+    <<-EOF
+<p>
+  #{eval(code)}
+</p>
+<pre>
+&lt;%= #{h code.strip.gsub("\n", "\n    ") } %&gt;
+</pre>
+    EOF
+  end
 end
